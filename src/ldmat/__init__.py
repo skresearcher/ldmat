13a7090e77a37c3a06fe8b235901b7b1e4c2b2e1
Shashaank N <shashaank04@gmail.com>
2025-08-01 22:18:54 -0400

    Make package methods accessible from import

--- src/ldmat/__init__.py
+++ src/ldmat/__init__.py
@@ -1,4 +1,6 @@
 from .__main__ import cli
+from .__main__ import convert_chromosome, convert_full_chromosome_h5, convert_maf_h5
+from .__main__ import select_submatrix_by_range, select_submatrix_by_list, select_submatrix_by_maf
 
 
 def run_cli():
